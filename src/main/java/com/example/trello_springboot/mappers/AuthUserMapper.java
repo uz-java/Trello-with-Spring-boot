6d670e8bcf822e3b668a34f5e4f8236ab7e2f925
asliddin <asliddintojaliyev88@gmail.com>
2022-09-11 14:29:54 +0500

    commit

--- src/main/java/com/example/trello_springboot/mappers/AuthUserMapper.java
+++ src/main/java/com/example/trello_springboot/mappers/AuthUserMapper.java
@@ -1,9 +1,17 @@
 package com.example.trello_springboot.mappers;
 
+import com.example.trello_springboot.domains.auth.AuthUser;
+import com.example.trello_springboot.dtos.user.AuthUSerDTO;
+import com.example.trello_springboot.dtos.user.AuthUserCreateDTO;
+import com.example.trello_springboot.dtos.user.AuthUserUpdateDTO;
+import org.mapstruct.Mapper;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/09/22 11:07 (Saturday)
  * Trello_Spring-boot/IntelliJ IDEA
  */
-public interface AuthUserMapper {
+@Mapper(componentModel = "spring")
+public interface AuthUserMapper extends BaseMapper<AuthUser, AuthUSerDTO, AuthUserCreateDTO, AuthUserUpdateDTO> {
+
 }
--- src/main/java/com/example/trello_springboot/mappers/BaseMapper.java
+++ src/main/java/com/example/trello_springboot/mappers/BaseMapper.java
@@ -1,9 +1,20 @@
 package com.example.trello_springboot.mappers;
 
+import java.util.List;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/09/22 11:08 (Saturday)
  * Trello_Spring-boot/IntelliJ IDEA
  */
-public interface BaseMapper {
+public interface BaseMapper<E, D, CD, UD> extends GenericMapper {
+    E fromDto(D dto);
+
+    E fromCreateDto(CD dto);
+
+    E fromUpdateDto(UD dto);
+
+    D toDto(E domain);
+
+    List<D> toDto(List<E> domains);
 }
